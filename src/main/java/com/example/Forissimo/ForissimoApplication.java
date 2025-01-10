da604c101c3caa65b203da3e4eb2174387dafa79
jauumfm <jauumfm@hotmail.com>
2025-01-10 19:46:01 -0300

    Initial commit

---
+++ src/main/java/com/example/Forissimo/ForissimoApplication.java
@@ -0,0 +1,13 @@
+package com.example.Forissimo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForissimoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForissimoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Forissimo/ForissimoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Forissimo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForissimoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
